200d398c6a6746c65c48354b3d8076d8361d6d68
jmonit <monitunleashed@gmail.com>
2025-11-18 10:34:07 +0000

    Update app.py text change

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("🎈 Mjs new app")
+st.write(
+    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+)
